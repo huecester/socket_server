a4389fd08f299dbd618552adc6d0256836b5c069
huecester <jericho.paulino@yahoo.com.ph>
2021-06-14 22:45:51 -0700

    TCP server

--- pkg/server/tcp/tcp_server.go
+++ pkg/server/tcp/tcp_server.go
@@ -1,24 +1,99 @@
-package tcp_server
+package tcp
 
 import (
 	"fmt"
+	"io"
 	"net"
 )
 
-func Server(port int, out chan<- net.Conn) {
+/////////////////
+// Connection  //
+/////////////////
+type Client struct {
+	server *server
+	conn   *net.Conn
+}
+
+// Constructor
+func newClient(serverPtr *server, connPtr *net.Conn) Client {
+	client := Client{
+		server: serverPtr,
+		conn:   connPtr,
+	}
+
+	return client
+}
+
+// Methods
+func (cl Client) Send(message string) {
+	conn := *cl.conn
+	conn.Write([]byte(message))
+}
+
+////////////
+// Server //
+////////////
+
+// Helper types
+type handler interface {
+	OnConnect(*Client)
+	OnMessage(*Client, string)
+}
+
+type server struct {
+	handler handler
+}
+
+// Constructor
+func New(handler handler) server {
+	return server{
+		handler: handler,
+	}
+}
+
+// Methods
+func (s server) receive(cl *Client) {
+	buffer := make([]byte, 1024)
+	conn := *cl.conn
+
+	for {
+		n, err := conn.Read(buffer)
+
+		if err != nil {
+			if err == io.EOF {
+				continue
+			}
+
+			fmt.Println(err)
+
+			break
+		}
+
+		hb := make([]byte, n)
+		copy(hb, buffer)
+		go s.handler.OnMessage(cl, string(hb))
+	}
+}
+
+func (s *server) Start(port int) error {
 	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		return err
 	}
+
+	defer ln.Close()
 
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			fmt.Println("Error:", err)
 			continue
 		}
 
-		out <- conn
+		cl := newClient(s, &conn)
+
+		go s.handler.OnConnect(&cl)
+		go s.receive(&cl)
 	}
+
+	return nil
 }
